bbbfb356b3cc35bace13fc8977c49abfe74e9cb7
alexeyShuncko <shuncko.a@yandex.ru>
2022-11-14 21:52:16 +0300

    Сообщение валидации

--- src/App.js
+++ src/App.js
@@ -5,16 +5,32 @@ function App() {
 
 
   const [value, setValue] = useState('')
+  const [edit, setEdit] = useState(false)
 
 
- const mySubmit =(e)=> {
-   e.preventDefault()
-   console.log(value);
- }
+  const mySubmit = (e) => {
+    e.preventDefault()
+    if (!value) {
+      setEdit(true)
+      document.querySelector('input').focus()
+    }
+    else {
+
+    }
+  }
+
+
+  const changeHandler = (e) => {
+    setEdit(false)
+    setValue(e.target.value)
+  }
 
   return (
     <form className='container' onSubmit={mySubmit}>
-      <input placeholder='Ваш номер...' value={value} onChange={(e)=> setValue(e.target.value)} />
+      <input placeholder='Ваш номер...' value={value} onChange={changeHandler} />
+      {edit &&
+        <div className='message'>Поле не должно быть пустым!</div>
+      }
       <button ><span>Заказать</span></button>
     </form>
   );
