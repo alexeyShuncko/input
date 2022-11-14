b56c8f5d5edfd5f7eb50a133b3358c198c739685
alexeyShuncko <shuncko.a@yandex.ru>
2022-11-14 21:44:56 +0300

    форма визуал

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './App.scss';
 
 function App() {
+
+
+  const [value, setValue] = useState('')
+
+
+ const mySubmit =(e)=> {
+   e.preventDefault()
+   console.log(value);
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className='container' onSubmit={mySubmit}>
+      <input placeholder='Ваш номер...' value={value} onChange={(e)=> setValue(e.target.value)} />
+      <button ><span>Заказать</span></button>
+    </form>
   );
 }
 
